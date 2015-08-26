5d4fd485d48e2dff7e9c5254182082a538a418f9
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-26 11:37:26 -0600

    Post comment data

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,21 +1,29 @@
 /*
- user clicks Button
- $.post
- Create actions
- Response
+ [X] user clicks Button
+ [X] $.post
+ [X] Create actions
+ [X] Response
  Add new comment to the UI
  */
 
 function submitComment(event) {
     event.preventDefault();
     console.log("hi, you submitted");
+    postCommentData();
 }
 
 function commentData() {
-    return {    author_name:  $("#comment_author_name").val(),
-                article_id: $("#comment_article_id").val(),
-                body: $("#comment_body").val();
+    return {
+        comment: {
+            author_name: $("#comment_author_name").val(),
+            article_id: $("#comment_article_id").val(),
+            body: $("#comment_body").val()
         }
+    };
+}
+
+function postCommentData() {
+    $.post("/comments", commentData());
 }
 
 $(document).ready(function () {
